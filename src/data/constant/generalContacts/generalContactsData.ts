033e1224ea054ac84c82a6cdd42a60707a5835c2
Slava Kamenev <slava-stavropol@yandex.ru>
2024-09-18 16:42:30 +0300

    change actual email

--- src/data/constant/generalContacts/generalContactsData.ts
+++ src/data/constant/generalContacts/generalContactsData.ts
@@ -7,11 +7,11 @@ const generalContactsData: generalContactsDataInterface = {
     whatsapp: 'https://wa.me/79219920152',
     vkontakte: 'https://vk.com/zagorodst',
   },
-  work_email: "mail@zagorodst.ru",
+  work_email: "zagorodst@yandex.ru",
   phone_number: "+78126432040",
   telegram_link: "https://t.me/sokolovpv",
   whatsapp_link: "https://wa.me/79219920152",
-  email: 'mail@zagorodst.ru',
+  email: 'zagorodst@yandex.ru',
   address: 'Санкт-Петербург, пр.\u00A0Просвещения, дом\u00A080, к.\u00A01, «Прометей», 3\u00A0этаж, офис\u00A03.1',
   workingHours: '**Пн-Вс с 9.00 до 21.00** без\u00A0обеда\u00A0и\u00A0выходных',
   mapLink: 'https://yandex.ru/map-widget/v1/?um=constructor%3A82d4640cfff3946c64d9620d25ce191b0ca06b56eea9ee8a0ad636757cd19e55&amp;source=constructor'
