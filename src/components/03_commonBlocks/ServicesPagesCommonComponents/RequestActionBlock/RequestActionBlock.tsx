b6cf72417477ae3c61c5b51b608c41fd8586d389
Slava Kamenev <slava-stavropol@yandex.ru>
2024-10-03 22:49:58 +0300

    add new InputForm forSeersPage

--- src/components/03_commonBlocks/ServicesPagesCommonComponents/RequestActionBlock/RequestActionBlock.tsx
+++ src/components/03_commonBlocks/ServicesPagesCommonComponents/RequestActionBlock/RequestActionBlock.tsx
@@ -4,7 +4,7 @@ import { RequestActionBlockInterface } from './RequestActionBlock.interface.ts';
 import { RequestPhoneInput } from '~features/Forms';
 
 
-const RequestActionBlock: React.FC<RequestActionBlockInterface> = ({ typePage, gridArea, nameForm }) => {
+const RequestActionBlock: React.FC<RequestActionBlockInterface> = ({ typePage, gridArea, color = 'dark', nameForm }) => {
   const callToActionText = {
     sellers: 'и получите бесплатную оценку своего объекта недвижимости уже\u00A0сегодня',
     buyers: 'и получите бесплатную консультацию специалиста',
@@ -13,21 +13,34 @@ const RequestActionBlock: React.FC<RequestActionBlockInterface> = ({ typePage, g
     legalSupport: 'и получите бесплатную консультацию специалиста',
   };
 
-  const gridAreaClassName = gridArea == 'first' ? styles.wrapper_first : styles.wrapper_second;
+  const setGridAreaClassName = gridArea == 'first' ? styles.wrapper_first : gridArea == 'second' ? styles.wrapper_second : styles.wrapper_third;
+  const setWrapperColorClassName = color == 'dark' ? styles.wrapper__color_dark : styles.wrapper__color_green;
+
 
   return (
-    <section className={`${styles.wrapper} ${gridAreaClassName}`}>
+    <section className={`${styles.wrapper} ${setGridAreaClassName} ${setWrapperColorClassName}`}>
       <div className={styles.container}>
 
-          <h2 className={styles.title}>
-            Оставьте заявку
-          </h2>
+        <h2 className={styles.title}>
+          {color == 'dark' ?
+            'Оставьте заявку'
+            :
+            'Фиксированная комиссия 4%'
+          }
+        </h2>
+        {color == 'dark' ?
           <p className={styles.description}>
             {callToActionText[typePage]}
           </p>
+          :
+          <p className={styles.smallDescription}>
+            Агентская комиссия оплачивается только в&nbsp;случае продажи объекта.
+            Для объектов до&nbsp;3&nbsp;млн. рублей комиссия составляет 120&nbsp;000 рублей.
+          </p>
+        }
 
         <div className={styles.formContainer}>
-          <RequestPhoneInput nameForm={nameForm} buttonText={'Отправить заявку'} />
+          <RequestPhoneInput nameForm={nameForm} buttonText={'Отправить заявку'} color={color == 'dark' ? 'common' : 'accent'} />
         </div>
       </div>
     </section>
